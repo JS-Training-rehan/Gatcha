5e42214b4b8b15f965cdb7075b326cf7eb25e672
RehanAjiNarwindo <rhnajin@gmail.com>
2023-11-28 21:34:12 +0700

    test

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
 // insert your code here
-var gacha
+// // insert your code here
+var gacha;
+var gacha = Math.floor(Math.random() * 6) + 1;
+
+switch (gacha) {
+  case 1:
+    console.log("coba lagi ya");
+    break;
+  case 2:
+    console.log("selamat kamu mendapatkan kupon sebanyak 5");
+    break;
+  case 3:
+    console.log("selamat kamu mendapatkan kupon sebanyak 15");
+    break;
+  case 4:
+    console.log("selamat kamu mendapatkan kupon sebanyak 50");
+    break;
+  case 5:
+    console.log("WOW, kamu menang jackpot! Selamat!!");
+    break;
+}
